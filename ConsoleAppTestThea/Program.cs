cb31fdda2bb821cc0101d7abdc7d1ee6962b6f03
Thea Braun <thea-braun@hotmail.com>
2022-08-06 15:37:06 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleAppTestThea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestThea
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+        }
+    }
+}
