108ccf774f973870362dbd1ae171acbbc06d5287
Thea Braun <thea-braun@hotmail.com>
2022-08-07 15:09:46 +0200

    New function for console

--- ConsoleAppTestThea/Program.cs
+++ ConsoleAppTestThea/Program.cs
@@ -10,8 +10,16 @@ namespace ConsoleAppTestThea
     {
         static void Main (string[] args)
         {
-            int input = Console.Read ();
-            Console.WriteLine (input);
+            //int input = Console.Read ();
+            //Console.WriteLine (input);
+            //Console.ReadLine ();
+
+            Console.Write ("Geben Sie Ihren Namen ein: ");
+            string name = Console.ReadLine ();
+            if (name == "")
+                Console.WriteLine ("Haben Sie keinen Namen?");
+            else
+                Console.WriteLine ($"Ihr Name ist \'{name}\'");
             Console.ReadLine ();
         }
     }
