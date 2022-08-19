193518870744970d7ecd3deb12c525a42c003fcd
Thea Braun <thea-braun@hotmail.com>
2022-08-19 16:53:55 +0200

    Update with new classes

---
+++ ConsoleAppTestThea/Classes/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestThea.Classes
+{
+    class Person
+    {
+        public string Name;
+        public string birthdate;
+
+        public Person (string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
--- ConsoleAppTestThea/Program.cs
+++ ConsoleAppTestThea/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppTestThea.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,12 +15,18 @@ namespace ConsoleAppTestThea
             //Console.WriteLine (input);
             //Console.ReadLine ();
 
-            Console.Write ("Geben Sie Ihren Namen ein: ");
-            string name = Console.ReadLine ();
-            if (name == "")
-                Console.WriteLine ("Haben Sie keinen Namen?");
-            else
-                Console.WriteLine ($"Ihr Name ist \'{name}\'");
+            //Console.Write ("Geben Sie Ihren Namen ein: ");
+            ////
+            //string name = Console.ReadLine ();
+            //if (name == "")
+            //    Console.WriteLine ("Haben Sie keinen Namen?");
+            //else
+            //    Console.WriteLine ($"Ihr Name ist \'{name}\'");
+            //Console.ReadLine ();
+            Person peter = new Person ("Peter");
+            peter.birthdate = "01.07.1944";
+            Console.WriteLine ("Person Name = "+peter.Name);
+            Console.WriteLine ("Person Geburtsdatum = " + peter.birthdate);
             Console.ReadLine ();
         }
     }
